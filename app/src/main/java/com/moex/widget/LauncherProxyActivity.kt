d8c9aaef14ec99817f380ac7c482fbced6e18b23
Paul Bunkie <paulbunkie@gmail.com>
2026-06-23 22:13:35 +0300

    Docs updated

---
+++ app/src/main/java/com/moex/widget/LauncherProxyActivity.kt
@@ -0,0 +1,11 @@
+package com.moex.widget
+
+import android.app.Activity
+import android.os.Bundle
+
+class LauncherProxyActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
